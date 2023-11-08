ed840ae97089d80271d08c04b139bfd581e1f921
mghenciu <52494172+mghenciu@users.noreply.github.com>
2023-11-08 11:01:37 +0200

    Adds the setup and first 3 demos

---
+++ backstop_data/engine_scripts/playwright/onReady.js
@@ -0,0 +1,8 @@
+module.exports = async (page, scenario, viewport, isReference, browserContext) => {
+  console.log('SCENARIO > ' + scenario.label);
+  await require('./clickAndHoverHelper')(page, scenario);
+
+  // add more ready handlers here...
+};
+
+window.scrollTo(0, document.body.scrollHeight);
---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,209 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Web_Agency_0_body_0_phone.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Web_Agency_0_body_0_phone.png",
+        "selector": "body",
+        "fileName": "backstop_default_Web_Agency_0_body_0_phone.png",
+        "label": "Web_Agency",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/web-agency-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Web_Agency_0_body_1_tablet.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Web_Agency_0_body_1_tablet.png",
+        "selector": "body",
+        "fileName": "backstop_default_Web_Agency_0_body_1_tablet.png",
+        "label": "Web_Agency",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/web-agency-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Web_Agency_0_body_2_desktop.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Web_Agency_0_body_2_desktop.png",
+        "selector": "body",
+        "fileName": "backstop_default_Web_Agency_0_body_2_desktop.png",
+        "label": "Web_Agency",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/web-agency-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Blogger_0_body_0_phone.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Blogger_0_body_0_phone.png",
+        "selector": "body",
+        "fileName": "backstop_default_Blogger_0_body_0_phone.png",
+        "label": "Blogger",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/blogger-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Blogger_0_body_1_tablet.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Blogger_0_body_1_tablet.png",
+        "selector": "body",
+        "fileName": "backstop_default_Blogger_0_body_1_tablet.png",
+        "label": "Blogger",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/blogger-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Blogger_0_body_2_desktop.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Blogger_0_body_2_desktop.png",
+        "selector": "body",
+        "fileName": "backstop_default_Blogger_0_body_2_desktop.png",
+        "label": "Blogger",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/blogger-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.017760416666666667,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 328
+        },
+        "diffImage": "../bitmaps_test/20231108-102730/failed_diff_backstop_default_Blogger_0_body_2_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Shop_0_body_0_phone.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Shop_0_body_0_phone.png",
+        "selector": "body",
+        "fileName": "backstop_default_Shop_0_body_0_phone.png",
+        "label": "Shop",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/shop-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Shop_0_body_1_tablet.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Shop_0_body_1_tablet.png",
+        "selector": "body",
+        "fileName": "backstop_default_Shop_0_body_1_tablet.png",
+        "label": "Shop",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/shop-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Shop_0_body_2_desktop.png",
+        "test": "../bitmaps_test/20231108-102730/backstop_default_Shop_0_body_2_desktop.png",
+        "selector": "body",
+        "fileName": "backstop_default_Shop_0_body_2_desktop.png",
+        "label": "Shop",
+        "misMatchThreshold": 0,
+        "url": "https://demosites.io/shop-gb/?optml_off=true",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 301
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
